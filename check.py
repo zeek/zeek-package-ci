ee1620ae7c3570b23daeb5c17433b9eb377eb490
Justin Azoff <jazoff@illinois.edu>
2018-04-25 14:40:00 -0400

    nicer cli

--- check.py
+++ check.py
@@ -5,6 +5,8 @@ import json
 import os
 import sys
 
+import argparse
+
 def main(pkg):
     print("Checking", pkg)
     results = check_all(pkg)
@@ -27,14 +29,32 @@ def main(pkg):
     return ok
 
 
-def json_main(pkg):
+def json_main(pkg, quiet=False, pretty=False):
+    indent = 4 if pretty else None
+
     res = check_all_dict(pkg)
-    print(json.dumps(res, indent=4))
+    if res["ok"] == False or not quiet:
+        print(json.dumps(res, indent=indent))
     return res["ok"]
 
-if __name__ == "__main__":
-    pkg = sys.argv[1]
-    if json_main(pkg):
+def main():
+
+    parser = argparse.ArgumentParser(description='Check bro packages')
+    parser.add_argument('package', metavar='N', type=str,
+                        help='bro package to check')
+    parser.add_argument('--quiet', dest='quiet', action='store_true',
+                        default=False,
+                        help='be quiet')
+    parser.add_argument('--pretty', dest='pretty', action='store_true',
+                        default=False,
+                        help='be pretty')
+
+    args = parser.parse_args()
+
+    if json_main(args.package, args.quiet, args.pretty):
         sys.exit(0)
     else:
         sys.exit(1)
+
+if __name__ == "__main__":
+    main()
